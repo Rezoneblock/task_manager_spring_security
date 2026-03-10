9a033db81de9ee26325a85a2b5803e2c9217eb46
Gordeev Timur <trew.tr2019@yandex.ru>
2026-03-10 22:13:32 +0300

    Fixed error text

--- src/main/java/com/gordeev/taskmanager/common/security/CustomUserDetailsService.java
+++ src/main/java/com/gordeev/taskmanager/common/security/CustomUserDetailsService.java
@@ -16,7 +16,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username).orElseThrow(() ->
-            new UsernameNotFoundException("Пользователя с именем")
+            new UsernameNotFoundException("Пользователя с именем '" + username + "' не существует")
         );
 
         return org.springframework.security.core.userdetails.User.builder()
